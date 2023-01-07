aa40d5ef4066711e0c43476c562b5b306b90dcb5
Simon McNeil <simonmcneil062@gmail.com>
2023-01-07 17:13:44 -0500

    Initial Commit

---
+++ simonmcneil_squareproject/simonmcneil_squareprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  simonmcneil_squareprojectApp.swift
+//  simonmcneil_squareproject
+//
+//  Created by Simon Mcneil on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct simonmcneil_squareprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
